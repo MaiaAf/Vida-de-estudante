c3f3aeece3d257ae7766fa9cd9cf9a5d1ed10565
MaiaAf <arthurmaia0125@gmail.com>
2025-02-14 20:43:09 -0300

    Deixar canvas pixelado em escala maior

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 720;
-canvas.height = 480;
 
 function desenharCenario() {
   ctx.fillStyle = "DarkGreen";

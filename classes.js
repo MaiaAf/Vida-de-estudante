03c1a8458c3200a659adaeafa102bde86d7a4dd7
MaiaAf <arthurmaia0125@gmail.com>
2025-02-16 00:14:57 -0300

    Refatorar sistema de animações e melhorar animação de andar

--- classes.js
+++ classes.js
@@ -18,24 +18,30 @@ class Sprite {
   }
 }
 
-class ImagemAnimada {
+class ImagemAnimada extends Sprite{
   constructor(pos, imagem) {
-    this.position = pos;
-    this.imagem = imagem;
+    super(pos);
+    this.imagem = new Image();
+    this.imagem.src = imagem;
     
     this.coluna = 0;
     this.linha = 0;
     this.estado = 1; // 0 - Parado, 1 - andando - 2 - pulando
     this.quadro_atual = 0;
     this.frame = 0;
-    this.anim_duracao = fps
+    this.frame_duracao = fps;
+    this.anim_frames= {
+      idle: [0,0,0,1,1,0,0,0,2,2],
+      andar: [0,1,2,3,4,5],
+    };
   }
 
   draw() {
+    this.animar()
     ctx.drawImage(
       this.imagem, // imagem
-      16 * this.linha,
       16 * this.coluna, // Posição x e y do início do recorte
+      16 * this.linha,
       16,
       16, // tamanho x e y do recorte
       this.position.x,
@@ -43,28 +49,32 @@ class ImagemAnimada {
       16,
       16,
     );
-    this.animar()
   }
 
   animar(){
-    this.coluna = this.estado
-    if (this.quadro_atual > this.anim_duracao) this.quadro_atual = 0;
+    this.linha = this.estado;
+    if (this.quadro_atual > this.frame_duracao) this.quadro_atual = 0;
 
     switch (this.estado) {
       case 0:
-        this.anim_duracao = fps * 3; // quantidade de frames na animação
-        if (![this.anim_duracao * 2 / 3, this.anim_duracao * 1 / 3].includes(this.quadro_atual)) break;
-        this.linha = this.frame % 3
+        this.frame_duracao = 60; // Duração do quadro em frames por segundo
+        if (this.quadro_atual != this.frame_duracao) break;
+
+        this.coluna = this.anim_frames.idle[this.frame % this.anim_frames.idle.length];
         break;
+
       case 1:
-        this.anim_duracao = fps / 6;
-        if (![this.anim_duracao].includes(this.quadro_atual)) break;
-        this.linha = this.frame % 3
+        this.frame_duracao = 6;
+        if (this.quadro_atual != this.frame_duracao) break;
+
+        this.coluna = this.anim_frames.andar[this.frame % this.anim_frames.andar.length];
+
+
         break;
       case 2:
-        this.anim_duracao = fps;
-        if (![this.anim_duracao].includes(this.quadro_atual)) break;
-        this.linha = this.frame % 2
+        this.frame_duracao = 12;
+        if (this.quadro_atual != this.frame_duracao) break;
+        this.coluna = this.frame % 2
         break;
       }
 
@@ -78,6 +88,7 @@ class ImagemAnimada {
       this.position.y += 3;
       this.estado = 2;
     } else {
+      this.position.x += 0.5;
       this.estado = 1;
     }
   }

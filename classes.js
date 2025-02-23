bf9f4280b2832207dc5ac829f755072ac7a112ef
MaiaAf <arthurmaia0125@gmail.com>
2025-02-23 12:54:32 -0300

    Remover classe sprite

--- classes.js
+++ classes.js
@@ -1,23 +1,3 @@
-class Sprite {
-  constructor(pos, color) {
-    this.position = pos;
-    this.color = color;
-  }
-
-  draw() {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.position.x, this.position.y, 10, 0, Math.PI * 2);
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  update() {
-    this.draw();
-    if (this.position.y < canvas.height - 20) this.position.y += 3;
-  }
-}
-
 class ImagemAnimada{
   constructor(pos, imagem) {
     this.position = pos;
